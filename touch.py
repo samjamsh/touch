46df53dce3cab231a0fc009dd4fe74aaa4976d0d
Yassir Daniel <75454542+samjamsh@users.noreply.github.com>
2023-08-17 14:09:33 +0200

    Update touch.py

--- touch.py
+++ touch.py
@@ -1,4 +1,4 @@
 def create_files(files):
  for file in files:
   with open(file,"w") as f: f.write("");
-import sys as system; help = f'use: {system.argv[0]} filename filename2 ...'; arguments = system.argv; arguments_lenght = len(arguments); system.exit(help) if len(arguments) == 1 else print('',end=''); parameters = arguments[1:]; files = parameters; create_files(files);
+import sys as system; help = f'use: {system.argv[0]} filename filename2 ...'; arguments = system.argv; arguments_lenght = len(arguments); system.exit(help) if len(arguments) == 1 else print('',end=''); files = arguments[1:]; create_files(files);
